5b303e2d798596987e00b0c39078d27c1db88348
wsc28 <wsc28@LAPTOP-K18318V6>
2019-09-13 13:20:21 +0800

    I have made a change

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,6 +4,8 @@
 public class Library {
 	
 	public static void main(String arg[]) {
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
 		System.out.println("When there is a fire, commit and push.");
 	}
 	
